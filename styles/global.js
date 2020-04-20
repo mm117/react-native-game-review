da3a6e33a67f6e96b19a5cf8b683da484b00de43
MishraMukesh117 <Mukesh.Mishra@accoliteindia.com>
2020-04-20 19:34:38 +0530

    changes for .apk build

--- styles/global.js
+++ styles/global.js
@@ -32,10 +32,10 @@ export const globalStyles = StyleSheet.create({
 
 export const images = {
   ratings: {
-    '1': require('../assets/rating-1.png'),
-    '2': require('../assets/rating-2.png'),
-    '3': require('../assets/rating-3.png'),
-    '4': require('../assets/rating-4.png'),
-    '5': require('../assets/rating-5.png'),
+    '1': require('../assets/images/rating-1.png'),
+    '2': require('../assets/images/rating-2.png'),
+    '3': require('../assets/images/rating-3.png'),
+    '4': require('../assets/images/rating-4.png'),
+    '5': require('../assets/images/rating-5.png'),
   }
 };
